28f740a819d81b5e69cb2668c81e5c2828014182
Osmany Jirón <osmany@senorcoders.com>
2018-11-30 17:35:24 -0600

    cambie la forma en que guarda los payload en la base de datos

--- api/controllers/NotificationsController.js
+++ api/controllers/NotificationsController.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  saveNotification: async (payload, type, user)=>{
-    payload = JSON.parse( JSON.stringify(payload) );
-    if(typeObject(payload.data) === "string"){
-        payload.data = JSON.parse(payload.data);
-    }
+  saveNotification: async (payload, type, user) => {
+    payload = JSON.parse(JSON.stringify(payload));
+    let is = payload.data.is;
+    payload.data = JSON.parse(payload.data.data);
+    payload.data.is = is;
     payload.title = payload.notification.title;
     payload.body = payload.notification.body;
     delete payload.notification;
