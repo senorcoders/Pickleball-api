02d03c44a15a6739852ce81fee02c5ccab32ef8c
Osmany Jirón <osmany@senorcoders.com>
2018-12-27 17:20:55 -0600

    agregue el new

--- api/controllers/CourtController.js
+++ api/controllers/CourtController.js
@@ -13,7 +13,7 @@ module.exports = {
         var db = Court.getDatastore().manager;
         var _court = db.collection(Court.tableName);
         let getCourts = function(coordinates, user){
-            user = mongoose.Types.ObjectId(user);
+            user = new mongoose.Types.ObjectId(user);
             return new Promise((resolve, reject) => {
                 _court.find({ coordinates, user })
                     .toArray(async (err, arr) => {
