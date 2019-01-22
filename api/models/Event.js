a04949ed9d647d3ce2d3a51828fc26a05a4541e9
Osmany Jirón <osmany@senorcoders.com>
2019-01-22 17:41:42 -0600

    agregue el api Event

---
+++ api/models/Event.js
@@ -0,0 +1,85 @@
+/**
+ * Event.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    name: {
+      type: "string",
+      required: true
+    },
+
+    description: {
+      type: "string",
+      required: false
+    },
+
+    date: {
+      type: "string",
+      columnType: "datetime",
+      required: true
+    },
+
+    time: {
+      type: "string",
+      columnType: "datetime",
+      required: true
+    },
+
+    partner: {
+      type: "string",
+      required: false
+    },
+
+    player: {
+      type: "string",
+      required: false
+    },
+
+    courts: {
+      type: "string",
+      required: false
+    },
+
+    matchTimes: {
+      type: "string",
+      required: false
+    },
+
+    travelInfo: {
+      type: "string",
+      required: false
+    },
+
+    eventStats: {
+      type: "string",
+      required: false
+    },
+
+    type: {
+      type: "string",
+      required: true
+    }
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
