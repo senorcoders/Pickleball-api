d2869aa8a89e3be0b4835348c1f36ce6e90f617f
Osmany Jirón <osmany@senorcoders.com>
2019-02-18 17:24:20 -0600

    agregue jsonwebtoken a auth de facebook

--- api/controllers/FacebookController.js
+++ api/controllers/FacebookController.js
@@ -1,7 +1,8 @@
+const jwt = require('jsonwebtoken')
 
 module.exports = {
     auth: catchErrors(async (req, res) => {
-        let facebook = req.body.facebook, _user =  req.body.user;
+        let facebook = req.body.facebook, _user = req.body.user;
         if (facebook.userID === null || facebook.userID === null) {
             res.status(400);
             return res.send("not found");
@@ -10,12 +11,22 @@ module.exports = {
         let user = await User.findOne({ userID: facebook.userID });
         if (user === undefined) {
             _user.userID = facebook.userID;
-            _user.email = _user.email || facebook.userID+"@pickleconnect.com";
+            _user.email = _user.email || facebook.userID + "@pickleconnect.com";
             _user.loginFacebook = facebook;
-            user = await User.create(_user).fetch(); 
+            user = await User.create(_user).fetch();
         }
+        let cargaUtil = user;
+        let secret = require("../../config/local").secretJwt;
+        jwt.sign(cargaUtil, secret, { expiresIn: 31536000 }, function (err, token) {
+            if (err) {
+                sails.log(err);
+                return res.json({ message: "User not found" });
+            }
+            // console.log(token);
+            user.token = token;
 
-        res.json(user);
+            res.json(user);
+        });
     })
 
 };
